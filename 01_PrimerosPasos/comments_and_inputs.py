b5f38d74a02e3a30cf1d6aa4ab0e24a732e8867e
sebastiansolar <sebastian.universitario.18@gmail.com>
2022-05-16 01:25:37 -0400

    se agrega primera sección del curso

---
+++ 01_PrimerosPasos/comments_and_inputs.py
@@ -0,0 +1,23 @@
+#comentarios 
+
+#Strings
+
+saludo = "Hola mundo" #Esto es un comentario en linea
+saludo2 = "Saludo 2"
+
+#print(saludo)
+print(saludo2)
+
+# Entrada desde teclado - input
+
+edad = int(input('Ingresa tu edad ---->  '))
+print(type(edad))
+
+#print("Tu edad es " + edad)
+edad += 1
+
+
+print(f"Tu edad es {edad}")
+
+nombre = input("Ingresa tu nombre")
+print("Hola ", nombre)
